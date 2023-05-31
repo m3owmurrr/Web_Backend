e348133ce63a268dfc3148b49f28ba51a8e495f6
Mik_Mftkhv <m.a.misha1605@gmail.com>
2023-05-31 05:14:23 +0300

    small fix x2

--- task_7/form.php
+++ task_7/form.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="task5.css">
+    <link rel="stylesheet" href="task7.css">
 </head>
 
 <body>
