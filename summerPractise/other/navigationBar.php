e080852e551fede1ff44cb91946409ba1d2d930b
Mik_Mftkhv <m.a.misha1605@gmail.com>
2023-06-10 02:15:37 +0300

    fix x4

--- summerPractise/other/navigationBar.php
+++ summerPractise/other/navigationBar.php
@@ -1,19 +1,19 @@
 <nav>
     <div class="d-flex justify-content-around">
         <div class="">
-            <li><a href="../summerPractise/index.php" title="Главная">Главная</a></li>
+            <li><a href="../../summerPractise/index.php" title="Главная">Главная</a></li>
         </div>
         <div class="">
-            <li><a href="../summerPractise/SecurityPosts/script.php" title="Посты охраны">Посты охраны</a></li>
+            <li><a href="../../summerPractise/SecurityPosts/script.php" title="Посты охраны">Посты охраны</a></li>
         </div>
         <div class="">
-            <li><a href="../summerPractise/Guards/script.php" title="Список охранников">Список охранников</a></li>
+            <li><a href="../../summerPractise/Guards/script.php" title="Список охранников">Список охранников</a></li>
         </div>
         <div class="">
-            <li><a href="../summerPractise/DutyLog/script.php" title="Журнал дежурства">Журнал дежурства</a></li>
+            <li><a href="../../summerPractise/DutyLog/script.php" title="Журнал дежурства">Журнал дежурства</a></li>
         </div>
         <div class="">
-            <li><a href="../summerPractise/ComplaintLog/script.php" title="Журнал замечаний">Журнал замечаний</a></li>
+            <li><a href="../../summerPractise/ComplaintLog/script.php" title="Журнал замечаний">Журнал замечаний</a></li>
         </div>
     </div>
 </nav>
