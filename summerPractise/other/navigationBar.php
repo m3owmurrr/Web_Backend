b3da4bc71970ef0924d51037bcfe753c40b6b3c4
Mik_Mftkhv <m.a.misha1605@gmail.com>
2023-06-10 02:05:32 +0300

    fix x1

--- summerPractise/other/navigationBar.php
+++ summerPractise/other/navigationBar.php
@@ -4,16 +4,16 @@
             <li><a href="../index.php" title="Главная">Главная</a></li>
         </div>
         <div class="">
-            <li><a href="../SecurityPosts/script.php" title="Посты охраны">Посты охраны</a></li>
+            <li><a href="./SecurityPosts/script.php" title="Посты охраны">Посты охраны</a></li>
         </div>
         <div class="">
-            <li><a href="../Guards/script.php" title="Список охранников">Список охранников</a></li>
+            <li><a href="./Guards/script.php" title="Список охранников">Список охранников</a></li>
         </div>
         <div class="">
-            <li><a href="../DutyLog/script.php" title="Журнал дежурства">Журнал дежурства</a></li>
+            <li><a href="./DutyLog/script.php" title="Журнал дежурства">Журнал дежурства</a></li>
         </div>
         <div class="">
-            <li><a href="../ComplaintLog/script.php" title="Журнал замечаний">Журнал замечаний</a></li>
+            <li><a href="./ComplaintLog/script.php" title="Журнал замечаний">Журнал замечаний</a></li>
         </div>
     </div>
 </nav>
