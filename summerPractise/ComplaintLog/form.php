e0341edb634577e45d5bca7c39847af8d98fe577
Mik_Mftkhv <m.a.misha1605@gmail.com>
2023-06-10 02:14:24 +0300

    fix x3

--- summerPractise/ComplaintLog/form.php
+++ summerPractise/ComplaintLog/form.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Управление записями</title>
+    <title>:Журнал событий</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
